6f4eaac899e25b2656b157f081aabea6ee4c7d3f
Jay Harris <jharris@harrisdesigns.com>
2012-01-17 13:04:02 -0500

    Added error handling for Morale errors

--- test/project-test.js
+++ test/project-test.js
@@ -10,14 +10,14 @@ vows.describe('Project API Tests').addBatch({
       topic: function(moraleApi) {
         var nockResponseData = [{
           project: {
-            id: 41,
+            id: 120,
             name: "Test Project #1",
             account_id: 1,
             updated_at: "2011-09-01T18:49:25Z",
             created_at: "2011-06-23T03:55:34Z"
           },
           project: {
-            id: 42,
+            id: 121,
             name: "Test Project #2",
             account_id: 1,
             updated_at: "2011-10-01T16:29:29Z",
@@ -46,7 +46,7 @@ vows.describe('Project API Tests').addBatch({
     },
     "retriving a project": {
       topic: function(moraleApi) {
-        var projectId = 41;
+        var projectId = 41200;
         var nockResponseData = {
           id: projectId,
           name: "Test Project #1",
@@ -69,7 +69,7 @@ vows.describe('Project API Tests').addBatch({
         assert.include(res, "account_id");
         assert.include(res, "id");
         assert.isNumber(res.id);
-        assert.equal(res.id, 41);
+        assert.equal(res.id, 41200);
       },
     },
     "adding a project": {
@@ -83,7 +83,7 @@ vows.describe('Project API Tests').addBatch({
           },
         };
         var nockResponseData = {
-          id: 42,
+          id: 42200,
           name: project.name,
           account_id: 1,
           updated_at: "2011-09-01T18:49:25Z",
@@ -108,41 +108,147 @@ vows.describe('Project API Tests').addBatch({
       },
     },
     "updating a project": {
-      topic: function(moraleApi) {
-        var project = {
-          id: 43,
-          name: "Test Project #4",
-        };
-        var nockRequestData = {
-          project: {
+      "with a project that exists": {
+        topic: function(moraleApi) {
+          var project = {
+            id: 43200,
+            name: "Test Project #4",
+          };
+          var nockRequestData = {
+            project: {
+              name: project.name,
+            },
+          };
+          var nockResponseData = {
+            id: project.id,
             name: project.name,
-          },
-        };
-        var nockResponseData = {
-          id: project.id,
-          name: project.name,
-          account_id: 1,
-          updated_at: "2011-09-01T18:49:25Z",
-          created_at: "2011-06-23T03:55:34Z"
-        };
+            account_id: 1,
+            updated_at: "2011-09-01T18:49:25Z",
+            created_at: "2011-06-23T03:55:34Z"
+          };
 
-        nock('https://valid-account.teammorale.com').put('/api/v1/projects/' + project.id, JSON.stringify(nockRequestData)).reply(200, JSON.stringify(nockResponseData), {
-          'content-type': 'application/json'
-        });
-        moraleApi.updateProject(project, this.callback);
+          nock('https://valid-account.teammorale.com').put('/api/v1/projects/' + project.id, JSON.stringify(nockRequestData)).reply(200, JSON.stringify(nockResponseData), {
+            'content-type': 'application/json'
+          });
+          moraleApi.updateProject(project, this.callback);
+        },
+        "should not return an error": function(res, err) {
+          assert.isNull(err);
+        },
+        "should return the updated project": function(res, err) {
+          assert.isObject(res);
+          assert.include(res, "name");
+          assert.isString(res.name);
+          assert.equal(res.name, "Test Project #4");
+          assert.include(res, "account_id");
+          assert.include(res, "id");
+          assert.isNumber(res.id);
+          assert.equal(res.id, 43200);
+        },
       },
-      "should not return an error": function(res, err) {
-        assert.isNull(err);
+      "with a project that does not exist": {
+        topic: function(moraleApi) {
+          var project = {
+            id: 43404,
+            name: "Test Project #4",
+          };
+          var nockRequestData = {
+            project: {
+              name: project.name,
+            },
+          };
+          var nockResponseData = {
+            error: "Project does not exist",
+          };
+
+          nock('https://valid-account.teammorale.com').put('/api/v1/projects/' + project.id, JSON.stringify(nockRequestData)).reply(404, JSON.stringify(nockResponseData), {
+            'content-type': 'application/json'
+          });
+          moraleApi.updateProject(project, this.callback);
+        },
+        "should not return data": function(res, err) {
+          assert.isNull(res);
+        },
+        "should return an error": function(res, err) {
+          assert.isObject(err);
+        },
+        "should return an http 404 status code": function(res, err) {
+          assert.isObject(err);
+          assert.include(err, "statusCode");
+          assert.isNumber(err.statusCode);
+          assert.equal(err.statusCode, 404);
+        },
+        "should return a Project Not Found error message": function(res, err) {
+          assert.isObject(err);
+          assert.include(err, "message");
+          assert.isString(err.message);
+          assert.equal(err.message, "Project does not exist");
+        },
       },
-      "should return the updated project": function(res, err) {
-        assert.isObject(res);
-        assert.include(res, "name");
-        assert.isString(res.name);
-        assert.equal(res.name, "Test Project #4");
-        assert.include(res, "account_id");
-        assert.include(res, "id");
-        assert.isNumber(res.id);
-        assert.equal(res.id, 43);
+    },
+    "deleting a project": {
+      "with a project that exists": {
+        topic: function(moraleApi) {
+          var projectId = 44200;
+
+          var nockResponseData = {
+            id: projectId,
+            name: "Sample Project That Was Deleted",
+            account_id: 1,
+            updated_at: "2011-09-01T18:49:25Z",
+            created_at: "2011-06-23T03:55:34Z"
+          };
+
+          nock('https://valid-account.teammorale.com').delete('/api/v1/projects/' + projectId).reply(200, JSON.stringify(nockResponseData), {
+            'content-type': 'application/json'
+          });
+          moraleApi.deleteProject(projectId, this.callback);
+        },
+        "should not return an error": function(res, err) {
+          assert.isNull(err);
+        },
+        "should return the updated project": function(res, err) {
+          assert.isObject(res);
+          assert.include(res, "name");
+          assert.isString(res.name);
+          assert.equal(res.name, "Sample Project That Was Deleted");
+          assert.include(res, "account_id");
+          assert.include(res, "id");
+          assert.isNumber(res.id);
+          assert.equal(res.id, 44200);
+        },
+      },
+      "with a project that does not exist": {
+        topic: function(moraleApi) {
+          var projectId = 44404;
+
+          var nockResponseData = {
+            error: "Project does not exist",
+          };
+
+          nock('https://valid-account.teammorale.com').delete('/api/v1/projects/' + projectId).reply(404, JSON.stringify(nockResponseData), {
+            'content-type': 'application/json'
+          });
+          moraleApi.deleteProject(projectId, this.callback);
+        },
+        "should not return data": function(res, err) {
+          assert.isNull(res);
+        },
+        "should return an error": function(res, err) {
+          assert.isObject(err);
+        },
+        "should return an http 404 status code": function(res, err) {
+          assert.isObject(err);
+          assert.include(err, "statusCode");
+          assert.isNumber(err.statusCode);
+          assert.equal(err.statusCode, 404);
+        },
+        "should return a Project Not Found error message": function(res, err) {
+          assert.isObject(err);
+          assert.include(err, "message");
+          assert.isString(err.message);
+          assert.equal(err.message, "Project does not exist");
+        },
       },
     },
   },
@@ -176,7 +282,7 @@ vows.describe('Project API Tests').addBatch({
     },
     "retriving a project": {
       topic: function(moraleApi) {
-        var projectId = 90;
+        var projectId = 51401;
         nock('https://invalid-account.teammorale.com').get('/api/v1/projects/' + projectId).reply(401, "", {
           'content-type': 'text/plain'
         });
@@ -238,7 +344,7 @@ vows.describe('Project API Tests').addBatch({
     "updating a project": {
       topic: function(moraleApi) {
         var project = {
-          id: 51,
+          id: 53401,
           name: "Project Should Not Get Created",
         };
         var nockRequestData = {
@@ -272,7 +378,7 @@ vows.describe('Project API Tests').addBatch({
     },
     "deleting a project": {
       topic: function(moraleApi) {
-        var projectId = 52;
+        var projectId = 54401;
         nock('https://invalid-account.teammorale.com').delete('/api/v1/projects/' + projectId).reply(401, "", {
           'content-type': 'text/plain'
         });
